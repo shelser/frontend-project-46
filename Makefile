5c7fb9bf9b7e929d30f773fbd8287195d2cf6cc1
shelser <shelserg@gmail.com>
2024-09-25 01:45:24 +0300

    add files: .gitignore, Makefile, bin/gendiff.js, package-lock.json, package.json, src/cli.js

---
+++ Makefile
@@ -0,0 +1,6 @@
+install:
+	npm ci
+publish:
+	npm publish --dry-run
+gendiff:
+	node bin/gendiff.js
---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import { program } from 'commander';
+
+export default () => {
+    program
+      .name('gendiff')
+      .version('0.0.1')
+      .description('Compares two configuration files and shows a difference.')
+      .helpOption('-h, --help', 'output usage information');
+    program.parse();
+};
