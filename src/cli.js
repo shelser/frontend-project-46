0c8633bb93566b04bc9d2c5725add5600a555c1b
shelser <shelserg@gmail.com>
2024-09-25 20:15:23 +0300

    modified file cli.js: added arguments and option

--- src/cli.js
+++ src/cli.js
@@ -5,6 +5,8 @@ export default () => {
       .name('gendiff')
       .version('0.0.1')
       .description('Compares two configuration files and shows a difference.')
-      .helpOption('-h, --help', 'output usage information');
+      .helpOption('-h, --help', 'output usage information')
+      .arguments('<filepath1> <filepath2>')
+      .option('-f, --format [type]', 'output format');
     program.parse();
 };
