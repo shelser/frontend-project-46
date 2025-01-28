21ed3f0def0707a60aae71d5d72ed25dcbdd5e83
shelser <shelserg@gmail.com>
2025-01-29 00:43:52 +0300

    changed parsers module and index module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+import { readFileSync } from 'fs';
+import { resolve, extname } from 'path';
+import { cwd } from 'process';
 import buildTree from './buildTree.js';
 import parsers from './parsers.js';
 import formatters from './formatters/index.js';
 
+const getData = (filePath) => {
+  const fullPath = resolve(cwd(), filePath);
+  const fileFormat = extname(filePath).slice(1);
+  return parsers[fileFormat](readFileSync(fullPath));
+};
+
 export default (filePath1, filePath2, format = 'stylish') => {
-  const data1 = parsers(filePath1);
-  const data2 = parsers(filePath2);
+  const data1 = getData(filePath1);
+  const data2 = getData(filePath2);
   const data = buildTree(data1, data2);
   return formatters[format](data);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,16 +1,7 @@
-import { readFileSync } from 'fs';
-import { resolve, extname } from 'path';
-import { cwd } from 'process';
 import yaml from 'js-yaml';
 
-export default (filePath) => {
-  const fullPath = resolve(cwd(), filePath);
-  const format = extname(filePath).slice(1);
-  const data = readFileSync(fullPath);
-  const parse = {
-    json: JSON.parse,
-    yaml: yaml.load,
-    yml: yaml.load,
-  };
-  return parse[format](data);
+export default {
+  json: JSON.parse,
+  yaml: yaml.load,
+  yml: yaml.load,
 };
