abc70088a4afded0df68fb291763132a1676c770
shelser <shelserg@gmail.com>
2025-01-28 11:16:33 +0300

    the test file has been changed

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,18 +11,7 @@ const formatsPlain = ['json', 'yml'];
 const formatsNested = ['json', 'yaml'];
 const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
 
-let expectedPlain;
-let expectedStylishFormat;
-let expectedPlainFormat;
-let expectedJsonFormat;
 const randomFormat = (arrFormats) => _.sample(arrFormats);
-
-beforeAll(() => {
-  expectedPlain = fs.readFileSync(getFixturePath('plainFileResult.txt'), 'utf-8');
-  expectedStylishFormat = fs.readFileSync(getFixturePath('stylishFormatResult.txt'), 'utf-8');
-  expectedPlainFormat = fs.readFileSync(getFixturePath('plainFormatResult.txt'), 'utf-8');
-  expectedJsonFormat = fs.readFileSync(getFixturePath('jsonFormatResult.txt'), 'utf-8');
-});
 
 test('test1: getting a string from plain .json files', () => {
   const file1Path = getFixturePath('plainFile1.json');
@@ -39,31 +28,31 @@ test('test2: getting a string from plain .yml files', () => {
 test('test3: random formats from plain files', () => {
   const file1Path1 = getFixturePath(`plainFile1.${randomFormat(formatsPlain)}`);
   const file1Path2 = getFixturePath(`plainFile2.${randomFormat(formatsPlain)}`);
+  const expectedPlain = fs.readFileSync(getFixturePath('plainFileResult.txt'), 'utf-8');
   const actual = genDiff(file1Path1, file1Path2);
-  console.log(actual);
   expect(actual).toEqual(expectedPlain.trim());
 });
 
 test('test4: matching with the stylish format result', () => {
   const file1Path1 = getFixturePath(`file1.${randomFormat(formatsNested)}`);
   const file1Path2 = getFixturePath(`file2.${randomFormat(formatsNested)}`);
+  const expectedStylishFormat = fs.readFileSync(getFixturePath('stylishFormatResult.txt'), 'utf-8');
   const actual = genDiff(file1Path1, file1Path2);
-  console.log(actual);
   expect(actual).toEqual(expectedStylishFormat.trim());
 });
 
 test('test5: matching with the plain format result', () => {
   const file1Path1 = getFixturePath(`file1.${randomFormat(formatsNested)}`);
   const file1Path2 = getFixturePath(`file2.${randomFormat(formatsNested)}`);
+  const expectedPlainFormat = fs.readFileSync(getFixturePath('plainFormatResult.txt'), 'utf-8');
   const actual = genDiff(file1Path1, file1Path2, 'plain');
-  console.log(actual);
   expect(actual).toEqual(expectedPlainFormat.trim());
 });
 
 test('test6: matching with the json format result', () => {
   const file1Path1 = getFixturePath(`file1.${randomFormat(formatsNested)}`);
   const file1Path2 = getFixturePath(`file2.${randomFormat(formatsNested)}`);
+  const expectedJsonFormat = fs.readFileSync(getFixturePath('jsonFormatResult.txt'), 'utf-8');
   const actual = genDiff(file1Path1, file1Path2, 'json');
-  console.log(actual);
   expect(actual).toEqual(expectedJsonFormat.trim());
 });
